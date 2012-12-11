50550eb39fd417988412abddbd05a7768db99c91
Sergey Timofiychuk <s.timofiychuk@gmail.com>
2012-12-11 21:41:20 +0200

    basic implementation of class javadoc generator

--- src/com/github/ideajavadocs/generator/impl/ClassJavaDocGenerator.java
+++ src/com/github/ideajavadocs/generator/impl/ClassJavaDocGenerator.java
@@ -9,12 +9,26 @@ public class ClassJavaDocGenerator implements JavaDocGenerator<PsiClass> {
 
     @Override
     public PsiDocComment generate(PsiClass element, boolean replace) {
-        String javaDoc =
-                "/**\n" +
-                "*\n" +
-                "*/";
+        PsiDocComment oldDocComment = null;
+        if (element.getFirstChild() instanceof PsiDocComment) {
+            oldDocComment = (PsiDocComment) element.getFirstChild();
+        }
+        if (oldDocComment != null) {
+
+            // TODO merge or replace javadoc
+        }
+
+        String name = element.getName();
+
+        StringBuilder result = new StringBuilder();
+        result
+                .append("/**\n")
+                .append("* The class of type ")
+                .append(name)
+                .append("*/");
+
         PsiElementFactory psiElementFactory = PsiElementFactory.SERVICE.getInstance(element.getProject());
-        PsiDocComment docElement = psiElementFactory.createDocCommentFromText(javaDoc);
+        PsiDocComment docElement = psiElementFactory.createDocCommentFromText(result.toString());
 
         return docElement;
     }
