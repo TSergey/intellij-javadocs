d1717bae561af3f396486fc0abd3a84ee5ed52ee
Sergiy Tymofiychuk <stymofiychuk@copyright.com>
2015-03-04 12:00:27 -0500

    add action to create/remove javadocs on project/directory/selected java classes

--- src/com/github/setial/intellijjavadocs/action/JavaDocsGenerateAction.java
+++ src/com/github/setial/intellijjavadocs/action/JavaDocsGenerateAction.java
@@ -1,7 +1,11 @@
 package com.github.setial.intellijjavadocs.action;
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.ide.highlighter.JavaFileType;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
 
@@ -13,7 +17,7 @@ import java.util.List;
  *
  * @author Sergey Timofiychuk
  */
-public class JavaDocsGenerateAction extends JavaDocGenerateAction {
+public class JavaDocsGenerateAction extends JavaDocGenerateAction implements DumbAware {
 
     /**
      * Instantiates a new Java docs generate action.
@@ -27,12 +31,42 @@ public class JavaDocsGenerateAction extends JavaDocGenerateAction {
      *
      * @param e the Event
      */
+    @Override
     public void actionPerformed(AnActionEvent e) {
         final PsiFile file = DataKeys.PSI_FILE.getData(e.getDataContext());
-        if (file == null) {
+        DataContext dataContext = e.getDataContext();
+
+        final Project project = CommonDataKeys.PROJECT.getData(dataContext);
+        final Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+        final VirtualFile[] files = CommonDataKeys.VIRTUAL_FILE_ARRAY.getData(dataContext);
+
+        if (editor != null && file != null) {
+            processFile(file);
+        } else if (project != null && files != null) {
+            processFiles(files, project);
+        } else {
             // TODO show message
-            return;
         }
+
+    }
+
+    private void processFiles(VirtualFile[] files, Project project) {
+        for (VirtualFile virtualFile : files) {
+            if (virtualFile.isDirectory()) {
+                processFiles(virtualFile.getChildren(), project);
+            } else {
+                PsiFile file = convertToPsiFile(virtualFile, project);
+                processFile(file);
+            }
+        }
+    }
+
+    private PsiFile convertToPsiFile(VirtualFile file, Project project) {
+        PsiManager manager = PsiManager.getInstance(project);
+        return manager.findFile(file);
+    }
+
+    private void processFile(PsiFile file) {
         List<PsiElement> elements = new LinkedList<PsiElement>();
         // Find all class elements
         List<PsiClass> classElements = getClasses(file);
@@ -44,6 +78,53 @@ public class JavaDocsGenerateAction extends JavaDocGenerateAction {
         for (PsiElement element : elements) {
             processElement(element);
         }
+    }
+
+    @Override
+    public void update(AnActionEvent event) {
+        Presentation presentation = event.getPresentation();
+        presentation.setEnabled(false);
+        DataContext dataContext = event.getDataContext();
+        Project project = CommonDataKeys.PROJECT.getData(dataContext);
+        if (project == null){
+            presentation.setEnabled(false);
+            return;
+        }
+        Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+
+        final VirtualFile[] files = CommonDataKeys.VIRTUAL_FILE_ARRAY.getData(dataContext);
+
+        if (editor != null) {
+            PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
+            if (file != null && JavaFileType.INSTANCE.equals(file.getFileType())) {
+                presentation.setEnabled(true);
+                return;
+            } else if (file != null && file.isDirectory()) {
+                presentation.setEnabled(true);
+                return;
+            }
+        } if (files!= null && containsJavaFiles(files)) {
+            presentation.setEnabled(true);
+            return;
+        }
+    }
+
+    private boolean containsJavaFiles(VirtualFile[] files) {
+        if (files == null) {
+            return false;
+        }
+        if (files.length < 1) {
+            return false;
+        }
+        boolean result = false;
+        for (VirtualFile file : files) {
+            if (file.isDirectory()) {
+                result = result || containsJavaFiles(file.getChildren());
+            } else if (JavaFileType.INSTANCE.equals(file.getFileType())) {
+                result = true;
+            }
+        }
+        return result;
     }
 
     /**
